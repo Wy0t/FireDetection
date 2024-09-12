e235c1a214ca2396f01ed9d3261d142fa7850f92
Lin Yu <t110ab0005@ntut.org.tw>
2024-09-12 13:35:26 +0800

    update backend

--- SP/app/serializers.py
+++ SP/app/serializers.py
@@ -1,1 +1,7 @@
 from rest_framework import serializers
+from .models import Video
+
+class VideoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Video
+        fields = ['id','file','title','uploaded_at']
